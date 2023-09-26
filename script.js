671929e8a662aa20688096f185e64393c7a3c541
Nityanand Tamsoy <nityanandtamsoy17@gmail.com>
2023-09-26 21:31:10 +0530

    commit

---
+++ script.js
@@ -0,0 +1,70 @@
+/*=============Dark Mode==========*/
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+
+/*=============Toggle icon navbar==========*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+/*==================scroll section active link====================*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
+
+    /*========sticky Navbar===========*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*==========remove toggle navbar & icon when clicked navbar link(scroll)============*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/*============scroll reveal===========*/
+ScrollReveal({
+    //reset:true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*===========typed js===========*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Full Stack Web Developer', 'Web Developer', 'Android App Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    typeDelay: 1000,
+    loop: true,
+});
+
